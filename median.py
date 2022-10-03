e9df7959298f824b0afd2bbfcb91d79c657837a4
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2022-10-03 10:29:58 +0100

    Done

--- median.py
+++ median.py
@@ -1,5 +1,4 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 while True:
     try:
@@ -9,4 +8,23 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort() #sorts the input in order to calcualte the median
+median = 0.0 #int
+#check if the array has an odd numbered amount of values or not and
+#calculate the median accordingly
+def calcMedian():
+    med = 0.0
+    #use to calc the two middle numbers
+    middle_lower = 0.0
+    middle_higher = 0.0
+    if len(numbers) % 2 == 0:
+        middle_lower = numbers[(len(numbers) // 2) - 1]
+        middle_higher = numbers[(len(numbers) // 2)]
+        med = (middle_lower + middle_higher) / 2
+    else:
+        med = numbers[ len(numbers) // 2 ]
+    return med
+
+median = calcMedian()
+print(median)
